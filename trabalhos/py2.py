38330c3c447a2e3fbd06506b176539c2894a6ae5
W-F-S <walkerf082@gmai.com>
2025-03-15 13:22:37 -0300

    aaa

--- trabalhos/py2.py
+++ trabalhos/py2.py
@@ -3,21 +3,36 @@ import numpy as np
 
 img = cv2.imread('Imagens/Lena.tif', 0) #lendo em escala de cinza
 
-# pegando proporcoes da imagem
-m, n = img.shape
 
-# criando mascara
-mascara = np.ones([3, 3], dtype = int)
-mascara = mascara / 9
+def filtro_media(img):
+	# pegando proporcoes da imagem
+	m, n = img.shape
+
+	img_new = np.zeros([m, n], dtype = int)
+	temp = []
+
+	for i in range(1, m-1):
+		for j in range(1, n-1):
+			temp.append(img[i-1, j-1])
+			temp.append(img[i-1, j])
+			temp.append(img[i-1, j+1])
+			temp.append(img[i, j-1])
+			temp.append(img[i, j])
+			temp.append(img[i, j+1])
+			temp.append(img[i+1, j-1])
+			temp.append(img[i+1, j])
+			temp.append(img[i+1, j+1])
+
+			tmp = (temp[3] +  temp[4]) / 2
+
+			temp = []
+
+			img_new[i, j]= tmp
 
 
-img_new = np.zeros([m, n])
 
-for i in range(1, m-1):
-	for j in range(1, n-1):
-		temp = img[i-1, j-1]*mascara[0, 0] + img[i-1, j]*mascara[0, 1] + img[i-1, j + 1] * mascara[0, 2] + img[i, j-1]*mascara[1, 0] + img[i, j]*mascara[1, 1] + img[i, j + 1]*mascara[1, 2] + img[i + 1, j-1]*mascara[2, 0] + img[i + 1, j]*mascara[2, 1] + img[i + 1, j + 1]*mascara[2, 2]
+	img_new = img_new.astype(np.uint8)
+	cv2.imwrite('out.tif', img_new)
 
-		img_new[i, j]= temp
 
-img_new = img_new.astype(np.uint8)
-cv2.imwrite('out.tif', img_new)
+filtro_media(img)
