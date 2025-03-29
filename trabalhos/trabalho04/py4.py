9899415923729fb026dfe7fee9cd72bc5815fbb1
W-F-S <walkerf082@gmai.com>
2025-03-29 18:11:14 -0300

    AAA

--- trabalhos/trabalho04/py4.py
+++ trabalhos/trabalho04/py4.py
@@ -20,49 +20,24 @@ g=T[f(x,y)]=a * log(f+i)
 g=a * (e^f - 1)
 g=a * raiz(f)
 
-tempos
+
 """
 
-img = cv2.imread('einstein.tif', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('sample.jpg')
 
-histograma = np.zeros(256, dtype=int)
+#econtrando esse a
+a = 255/(np.log(1 + np.max(img)))
 
-m, n = img.shape
-
-img_new =  np.zeros([m, n], dtype = int)
-
-for i in range(m):
-    for j in range(n):
-        histograma[img[i, j]] += 1  # Contabiliza os pixels
+print(a)
+exit()
 
 
-
-cdf = np.zeros(256, dtype=int)
-
-cdf[0] = histograma[0]
-
-#calculando cdf
-for i in range(1, 256):
-    cdf[i] = cdf[i - 1] + histograma[i] # frequência atual do histograma + valor acumulado anterior.
-
-
-cdf_min=1
-# menor valor diferente de zero
-for i in range(len(cdf)):
-    if(cdf[i] < cdf_min and cdf[i]!=0):
-        cdf_min = cdf[i]
-
-total_pixels = m * n
-
-# Normalizando a CDF
-for i in range(256):
-        cdf[i] = ((cdf[i] - cdf_min) / (total_pixels - cdf_min) * 255)
-
+transformacao_log = a * np.log(1 + img)
+transformacao_log = np.array(transformacao_log, dtype = np.uint8)
 
 for i in range(m):
     for j in range(n):
-        #print(f"{i},{j}={str(cdf[img[i, j]])}")
-        img_new[i, j] = cdf[img[i, j]]
+        img_new[i, j] = transformacao_log[i,j]
 
 print(img_new)
 cv2.imwrite('equalizado.tif', img_new)
